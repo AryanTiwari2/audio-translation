300074f476922fc63fb62adfab4c5afc3a719e09
Aryan <at218808@gmail.com>
2024-09-14 11:49:40 +0530

    fixed play audio

--- src/components/PlayAudio.jsx
+++ src/components/PlayAudio.jsx
@@ -1,51 +1,42 @@
-import React,{useRef,useState} from 'react'
+import React,{useRef,useState,useEffect} from 'react'
+import WavesurferPlayer from '@wavesurfer/react'
 
 const PlayAudio = (props) => {
     const {setIsCorrectAudio,audioURL} = props;
-    const audioRef = useRef(null);
-    const [isPlaying, setIsPlaying] = useState(false);
-    const [currentTime, setCurrentTime] = useState(0);
-    const [duration, setDuration] = useState(0);
-  
-    const togglePlayPause = () => {
-      if (isPlaying) {
-        audioRef.current.pause();
-      } else {
-        audioRef.current.play();
-      }
-      setIsPlaying(!isPlaying);
-    };
-  
-    const handleLoadedMetadata = () => {
-      setDuration(audioRef.current.duration);
-    };
-  
-    const handleTimeUpdate = () => {
-      setCurrentTime(audioRef.current.currentTime);
-    };
+    const [wavesurfer, setWavesurfer] = useState(null)
+    const [isPlaying, setIsPlaying] = useState(false)
+
+  const onReady = (ws) => {
+    setWavesurfer(ws)
+    setIsPlaying(false)
+  }
+
+  const togglePlayPause = () => {
+    wavesurfer && wavesurfer.playPause()
+  }
   
     return (
         <>
-      <div className="flex justify-center flex-col mt-16">
-        <audio
-          ref={audioRef}
-          src={audioURL}
-          onLoadedMetadata={handleLoadedMetadata}
-          onTimeUpdate={handleTimeUpdate}
-        />
-        </div>
-        <div className='flex justify-center'>
-        <i className={`text-[6.5rem] md:text-[8.5rem] fa-solid fa-headphones ${isPlaying ? "animate":""}`}></i>  
-        </div>
-        <div className='flex justify-center mt-4 md:mt-8 gap-6 md:gap-16'>
-            <button onClick={()=>{window.location.href="/audio-translation"}} className='bg-white text-black pl-4 pr-4 pt-1 pb-1 shadow-sm rounded-xl'>Reject</button>
-            <button onClick={togglePlayPause} className='bg-red-400 pl-4 pr-4 rounded-2xl text-white'> 
+       <div className='mt-4 ml-8 mr-8 sm:ml-[100px] sm:mr-[100px]'>
+       <WavesurferPlayer
+        height={100}
+        waveColor="#60a5fa"
+        url={audioURL}
+        onReady={onReady}
+        onPlay={() => setIsPlaying(true)}
+        onPause={() => setIsPlaying(false)}
+      />
+       </div>
+
+        <div className='flex justify-center mt-4'>
+            <button onClick={()=>{window.location.href="/audio-translation"}} className='flex gap-1 items-center justify-center bg-white sm:w-[15%] md:w-[20%] w-[30%] text-black pl-4 pr-4 pt-1 pb-1 shadow-sm rounded-l-xl hover:bg-red-300 hover:text-white'><i className="fa-solid fa-rotate-right"></i>Retry</button>
+            <button onClick={togglePlayPause} className='bg-blue-400 pl-4 pr-4 text-white'> 
             {isPlaying ? 
             <i className="fa-solid fa-pause"></i>:
             <i className="fa-solid fa-play"></i>
             }
             </button>
-            <button onClick={()=>{setIsCorrectAudio(true)}} className='bg-blue-500 text-text pl-4 pr-4 pt-1 pb-1 shadow-sm rounded-xl'> Accept</button>
+            <button onClick={()=>{setIsCorrectAudio(true)}} className='flex gap-1 items-center justify-center bg-white sm:w-[15%] md:w-[20%] w-[30%] text-text pl-4 pr-4 pt-1 pb-1 shadow-sm rounded-r-xl hover:bg-green-300 hover:text-white'>Transcribe</button>
         </div>
       </>
     );
